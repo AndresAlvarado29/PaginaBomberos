2bf6b4b2a8300ff0482dd021a1f966a5668bc49d
AndresAlvarado9 <nalvaradoc1@est.ups.edu.ec>
2024-01-11 21:19:11 -0500

    actualizacion en el json del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { ScrollDispatcher } from '@angular/cdk/scrolling';
 
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,51 +12,12 @@ import { ScrollDispatcher } from '@angular/cdk/scrolling';
 })
 export class AppComponent {
   title = 'Bomberos Oña';
-  elementosAnimados!: NodeListOf<Element>;
-  animationTimeout: any;
+  
   constructor(private scrollDispatcher: ScrollDispatcher){
 
   }
-
-  ngAfterViewInit() {
-    this.elementosAnimados = document.querySelectorAll('.info-unidad');
-    console.log("perra hola:"+this.elementosAnimados)
-    console.log('Elementos encontrados:', this.elementosAnimados.length);
-    this.checkElementsVisibility();
-    console.log("hola perra aqui estoy"+this.checkElementsVisibility())
-  }
-  noticias=true;
+ noticias=true;
   carrusel=true;
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event) {
-    if(this.animationTimeout){
-      clearTimeout(this.animationTimeout);
-    }
-    this.animationTimeout=setTimeout(()=>{
-      this.checkElementsVisibility();
-    },100);
-  }
-
-  checkElementsVisibility() {
-    this.elementosAnimados.forEach((elemento: Element) => {
-      if (this.isElementInViewport(elemento as HTMLElement)) {
-        elemento.classList.add('animacion1');
-      } else {
-        elemento.classList.remove('animacion1');
-      }
-    });
-  }
-
-  private isElementInViewport(element: HTMLElement): boolean {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
 
   ocultar(){
     this.carrusel=false;
