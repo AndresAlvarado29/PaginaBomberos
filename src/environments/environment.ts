3549cb1e3e2db4c2c17d8feeab3ebbfa981447ae
Andres Alvarado <nalvaradoc1@est.ups.edu.ec>
2024-08-28 16:05:55 -0500

    cambio del gitignore y environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
+import { llaves } from "src/apikeys/llaves";
 export const environment = {
-    keyMailService:'service_06yzrfy',
-    keyTemplateService:'template_nh88way',
-    PublicKey:'I-Dyhbfn7psx-_7Dd',
-    key:"An`&r3S@LvC411E~",
-    nv:'hola'
+    keyMailService:llaves.keyMailService,
+    keyTemplateService: llaves.keyTemplateService,
+    PublicKey: llaves.PublicKey,
+    key:llaves.key,
 };
