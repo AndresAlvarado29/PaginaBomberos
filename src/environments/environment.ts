163f99299788dd6572261d4aafe8ba077549ef59
Andres Alvarado <nalvaradoc1@est.ups.edu.ec>
2024-08-28 15:54:11 -0500

    cambio del readme y el gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     keyMailService:'service_06yzrfy',
     keyTemplateService:'template_nh88way',
     PublicKey:'I-Dyhbfn7psx-_7Dd',
-    key:"An`&r3S@LvC411E~"
+    key:"An`&r3S@LvC411E~",
+    nv:'hola'
 };
